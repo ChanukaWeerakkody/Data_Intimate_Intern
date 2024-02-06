806e5a8eba3e2370af75d59073c4807b13b6297f
ChanukaWeerakkody <chanuka.weerakkody123@gmail.com>
2024-02-06 16:04:10 +0530

    add ejs and sendmailer dependencies to send email

--- controller/user.controller.ts
+++ controller/user.controller.ts
@@ -1,12 +1,60 @@
+import path from "path";
+
+require('dotenv').config();
 import {Response,Request,NextFunction} from "express";
-import userModel,{IUser} from "../models/user.model";
+import userModel from "../models/user.model";
 import ErrorHandler from "../util/ErrorHandler";
 import {CatchAsyncError} from "../middleware/catchAsyncErrors";
+import jwt,{Secret} from "jsonwebtoken";
+import * as ejs from "ejs";
 
 //Register a new user
-interface IRegisterUser{
+interface IRegisterBody{
     name:string;
     email:string;
     password:string;
     avatar?:string;
+}
+
+export const registrationUser = CatchAsyncError(async (req:Request,res:Response,next:NextFunction)=>{
+    try{
+        const {name,email,password} = req.body;
+
+        const isEmailExists = await userModel.findOne({email});
+        if(isEmailExists){
+            return next(new ErrorHandler("Email already exists",400));
+        }
+
+        const user:IRegisterBody = {
+            name,
+            email,
+            password,
+        };
+
+        const activationToken = createActivationToken(user);
+
+        const activationCode = activationToken.activationCode;
+
+        const data ={user:{name:user.name},activationCode};
+        const html = ejs.renderFile(path.join(__dirname,"../mails/activation-mail.ejs"),data);
+
+    }catch (error:any){
+        return next(new ErrorHandler(error.message,400));
+    }
+})
+
+interface IActivationToken{
+    token:string;
+    activationCode:string;
+}
+
+export const createActivationToken = (user:any)=>{
+    const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+
+    const token = jwt.sign({
+            user,activationCode
+        },process.env.ACTIVATION_SECRET as Secret,{
+            expiresIn:"5m"
+        });
+    return{token,activationCode};
 }
